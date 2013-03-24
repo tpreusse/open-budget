1a51206add970a70c6060eba5183a8f62e5d08a8
Thomas Preusse <t@preus.se>
2013-03-24 12:24:13 +0100

    make s3 bucket region configurable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
       :provider                 => 'AWS',
       :aws_access_key_id        => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key    => ENV['S3_SECRET'],
-      :region                   => 'eu-west-1'
+      :region                   => ENV['S3_REGION']
     }
     config.fog_directory    = ENV['S3_BUCKET']
 
